cd2dad551de35e43584ec93736b2ad617a25b5f8
Rodion Maltsev <rodionmaltsev1999@gmail.com>
2025-12-23 16:44:07 +0300

    add deploy

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
-import * as deepar from "deepar";
-import Carousel from "./carousel.js";
+import * as deepar from 'deepar';
+import Carousel from './carousel.js';
 
 // Log the version. Just in case.
-console.log("Deepar version: " + deepar.version);
+console.log('Deepar version: ' + deepar.version);
 
 // Top-level await is not supported.
 // So we wrap the whole code in an async function that is called immediatly.
 (async function () {
   // Get the element you want to place DeepAR into. DeepAR will inherit its width and height from this and fill it.
-  const previewElement = document.getElementById("ar-screen");
+  const previewElement = document.getElementById('ar-screen');
 
   // trigger loading progress bar animation
-  const loadingProgressBar = document.getElementById("loading-progress-bar");
-  loadingProgressBar.style.width = "100%";
+  const loadingProgressBar = document.getElementById('loading-progress-bar');
+  loadingProgressBar.style.width = '100%';
 
   // All the effects are in the public/effects folder.
   // Here we define the order of effect files.
   const effectList = [
-    "effects/ray-ban-wayfarer.deepar",
-    "effects/viking_helmet.deepar",
-    "effects/MakeupLook.deepar",
-    "effects/Split_View_Look.deepar",
-    "effects/flower_face.deepar",
-    "effects/Stallone.deepar",
-    "effects/galaxy_background_web.deepar",
-    "effects/Humanoid.deepar",
-    "effects/Neon_Devil_Horns.deepar",
-    "effects/Ping_Pong.deepar",
-    "effects/Pixel_Hearts.deepar",
-    "effects/Snail.deepar",
-    "effects/Hope.deepar",
-    "effects/Vendetta_Mask.deepar",
-    "effects/Fire_Effect.deepar",
+    'effects/ray-ban-wayfarer.deepar',
+    'effects/viking_helmet.deepar',
+    'effects/MakeupLook.deepar',
+    'effects/Split_View_Look.deepar',
+    'effects/flower_face.deepar',
+    'effects/Stallone.deepar',
+    'effects/galaxy_background_web.deepar',
+    'effects/Humanoid.deepar',
+    'effects/Neon_Devil_Horns.deepar',
+    'effects/Ping_Pong.deepar',
+    'effects/Pixel_Hearts.deepar',
+    'effects/Snail.deepar',
+    'effects/Hope.deepar',
+    'effects/Vendetta_Mask.deepar',
+    'effects/Fire_Effect.deepar',
   ];
 
   let deepAR = null;
@@ -39,13 +39,13 @@ console.log("Deepar version: " + deepar.version);
   // Initialize DeepAR with an effect file.
   try {
     deepAR = await deepar.initialize({
-      licenseKey: "your_license_key_goes_here",
+      licenseKey: process.env.LICENSE_KEY,
       previewElement,
       effect: effectList[0],
       // Removing the rootPath option will make DeepAR load the resources from the JSdelivr CDN,
       // which is fine for development but is not recommended for production since it's not optimized for performance and can be unstable.
       // More info here: https://docs.deepar.ai/deepar-sdk/platforms/web/tutorials/download-optimizations/#custom-deployment-of-deepar-web-resources
-      rootPath: "./deepar-resources",
+      rootPath: './deepar-resources',
       additionalOptions: {
         cameraConfig: {
           // facingMode: 'environment'  // uncomment this line to use the rear camera
@@ -54,26 +54,26 @@ console.log("Deepar version: " + deepar.version);
     });
   } catch (error) {
     console.error(error);
-    document.getElementById("loading-screen").style.display = "none";
-    document.getElementById("permission-denied-screen").style.display = "block";
+    document.getElementById('loading-screen').style.display = 'none';
+    document.getElementById('permission-denied-screen').style.display = 'block';
     return;
   }
 
   // Hide the loading screen.
-  document.getElementById("loading-screen").style.display = "none";
-  document.getElementById("ar-screen").style.display = "block";
+  document.getElementById('loading-screen').style.display = 'none';
+  document.getElementById('ar-screen').style.display = 'block';
 
   window.effect = effectList[0];
 
-  const glassesCarousel = new Carousel("carousel");
+  const glassesCarousel = new Carousel('carousel');
   glassesCarousel.onChange = async (value) => {
-    const loadingSpinner = document.getElementById("loading-spinner");
+    const loadingSpinner = document.getElementById('loading-spinner');
 
     if (window.effect !== effectList[value]) {
-      loadingSpinner.style.display = "block";
+      loadingSpinner.style.display = 'block';
       await deepAR.switchEffect(effectList[value]);
       window.effect = effectList[value];
     }
-    loadingSpinner.style.display = "none";
+    loadingSpinner.style.display = 'none';
   };
 })();
